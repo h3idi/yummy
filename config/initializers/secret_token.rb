c889e6ccc1615f03f39c36f6593853d4abdb70b8
h3idi <heidi.harman@gmail.com>
2013-05-18 06:47:21 +0100

    initial push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yummy::Application.config.secret_token = 'c06745981413aa35e046ab89313bb766fc37f09e2103aba9dff091c0e205264f32d77fa4ddd35cc31d7743c2374f3e9fbeedf6e813db197c0c542c8e9c1211b5'
